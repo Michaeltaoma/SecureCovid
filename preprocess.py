1cd8be98849747a9c3607354ca7e15be3898fa03
michaelma <16tm43@queensu.ca>
2022-03-12 18:02:29 -0800

    attack training set

--- preprocess.py
+++ preprocess.py
@@ -46,3 +46,21 @@ def load_split_train_test(datadir, valid_size=.2):
     testloader = torch.utils.data.DataLoader(test_data,
                                              sampler=test_sampler, batch_size=8)
     return trainloader, testloader, dataset_size
+
+
+def load_all_train(datadir, valid_size=0.0):
+    train_data = datasets.ImageFolder(datadir,
+                                      transform=preprocess.data_transforms['train'])
+    test_data = datasets.ImageFolder(datadir,
+                                     transform=preprocess.data_transforms['val'])
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+    np.random.shuffle(indices)
+    train_idx, test_idx = indices[split:], indices[:split]
+    dataset_size = {"train": len(train_idx), "val": len(test_idx)}
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+    trainloader = torch.utils.data.DataLoader(train_data,
+                                              sampler=train_sampler, batch_size=1)
+    return trainloader
